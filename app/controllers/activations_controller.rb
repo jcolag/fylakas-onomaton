8f1617897e04d493cce5b009730650195684bbdd
John Colagioia <john@colagioia.net>
2021-07-05 06:06:05 -0400

    Add show API to confirm activation

--- app/controllers/activations_controller.rb
+++ app/controllers/activations_controller.rb
@@ -1,5 +1,5 @@
 class ActivationsController < ApplicationController
-  before_action :set_activation, only: %i[ edit update destroy ]
+  before_action :set_activation, only: %i[ show edit update destroy ]
 
   # GET /activations or /activations.json
   def index
@@ -8,19 +8,31 @@ class ActivationsController < ApplicationController
 
   # GET /activations/1 or /activations/1.json
   def show
+    result = {
+      params: params,
+      code: @activation.code,
+      result: @activation.activated
+    }
+    json = result.to_json
+
+    respond_to do |format|
+      format.html { render :show, location: @activation }
+      format.json { render json: json }
+    end
   end
 
   # GET /activations/new
   def new
     code, json = generate_code 5, params
-    Activation.new({
+    act = Activation.new({
       code: code,
       device_info: params['device']
-    }).save
+    })
+    act.save
 
     respond_to do |format|
       format.html { redirect_to action: :show }
-      format.json { render json: json, status: :ok }
+      format.json { render json: json.gsub(/"000"/, act.id.to_s), status: :ok }
     end
   end
 
@@ -88,7 +100,7 @@ class ActivationsController < ApplicationController
     def generate_code(length, params)
       letters = Array('A'..'Z') + Array('0'..'9')
       code = Array.new(length) { letters.sample }.join
-      result = { code: code, params: params, result: :ok }
+      result = { code: code, params: params, result: :ok, id: '000' }
       return code, result.to_json
     end
 end
