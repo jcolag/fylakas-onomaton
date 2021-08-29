6be7d15a3b5b0a1305cf265b6e4137fb8aa652de
John Colagioia <john@colagioia.net>
2021-08-29 06:36:14 -0400

    Fix migration so that it doesn't contradict itself

--- db/migrate/20210629205708_add_activated_to_activations.rb
+++ db/migrate/20210629205708_add_activated_to_activations.rb
@@ -4,7 +4,7 @@
 class AddActivatedToActivations < ActiveRecord::Migration[6.0]
   def change
     add_column :activations, :activated, :string
-    add_reference :activations, :user, foreign_key: true
+    add_reference :activations, :user
     add_foreign_key :activations, :users
   end
 end
